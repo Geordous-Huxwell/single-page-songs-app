ac130652d5f0d89723f657ca05ec362179157dd8
Geordous-Huxwell <jconl266@mtroyal.ca>
2022-10-16 19:01:25 -0600

    heroku go-live test4

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
 <?php
-require_once "./jconl266/index.php"
+require_once './main.php';
+
 ?>
+
+<html> 
+  
+  <?php
+generateHeader();
+?>
+
+    <article>
+        <!-- <h2>Song Info</h2> -->
+        <details>
+          <summary>
+            <span class="title"><?=$title?></span>
+            <span class="artist"><?=$artist?></span>
+          </summary>
+          <h3>
+          <ul>
+            <!-- TODO: Harshad - make this list into a function and style the list items-->
+            <li>Year: <?=$year?></li>
+            <li>Length: <?=$duration?></li>
+            <li>Genre: <?=$genre?></li>
+            <li>Type: <?=$artistType?></li>
+          </ul>
+          </h3>
+          
+        </details>
+        <div class="grid">
+          
+          <?php generateMetrics($metrics); ?>
+          
+        </div>
+    </article>
+    
+  </body>
+
+   <!-- TODO: Harshad - refer to the assignment instructions for what content 
+    should be in the footer and build a function to generate it so it can be 
+    injected into all pages -->
+
+  <?php
+    generateFooter();
+    ?>
+</html>
--- search.php
+++ search.php
@@ -1,5 +1,5 @@
 <?php
-require_once './jconl266/main.php';
+require_once './main.php';
 // echo json_encode($artistDB);
 ?>
 <style>
