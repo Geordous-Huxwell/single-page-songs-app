63ac426620f6e34f3583bcab3801d9e8e8e0c4d0
Geordous-Huxwell <jconl266@mtroyal.ca>
2022-10-16 19:07:50 -0600

    atyle.css path correction

--- main.php
+++ main.php
@@ -67,7 +67,7 @@ function generateHeader()
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>COMP 3512 Assign1</title>
-        <link rel="stylesheet" href="/css/style.css">
+        <link rel="stylesheet" href="./css/style.css">
       </head>
 
       <header> COMP 3512 Assign1
